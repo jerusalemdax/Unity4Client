634c9f992868431f62bf622daef5767b5e7c9776
jerusalemdax <jerusalemdax@gmail.com>
2016-04-06 11:12:08 +0800

    origin project

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    Debug.Log("Unity version:" + Application.unityVersion);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
