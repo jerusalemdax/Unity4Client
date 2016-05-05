98a23742530b6696fb7d22e9b0df4b5012f651c6
jerusalemdax <jerusalemdax@gmail.com>
2016-05-05 11:03:45 +0800

    fix mac编译脚本失败

---
+++ Scripts/BuildScripts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xbuild /t:Build /p:Configuration=Release /p:CommonDllPath=/Applications/Unity4/Unity.app/Contents/Frameworks/Managed /p:UIDllPath=/Applications/Unity4/Unity.app/Contents/UnityExtensions/Unity/GUISystem/4.7.1
